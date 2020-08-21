9f80168456f7f0c245e9f5cabc19082cf5afc9f2
Francesco Ballarin <francesco.ballarin@sissa.it>
2020-08-21 16:14:51 +0200

    Add trailing slash for sitemap

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ html_theme_options = {
 
     # Specify a base_url used to generate sitemap.xml. If not
     # specified, then no sitemap will be built.
-    "base_url": "https://www.rbnicsproject.org",
+    "base_url": "https://www.rbnicsproject.org/",
 
     # Set the color and the accent color
     "theme_color": "#065895",
